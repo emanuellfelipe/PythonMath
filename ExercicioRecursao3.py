5f595caf72c905709773d2f455de8167aa6c1d4b
Emanuel Laureano <97695747+emanuellfelipe@users.noreply.github.com>
2023-06-27 23:04:34 -0300

    Add files via upload

--- ExercicioRecursao3.py
+++ ExercicioRecursao3.py
@@ -4,10 +4,10 @@ m =  int(input("Insira o Valor de m: "))
 
 def exp_mod(a, n, m):
     if m < 2:
-        raise ValueError("É impossível calcular com m < 2")
+        return "É impossível calcular com m < 2"
     if n == 0:
         if a == 0:
-            raise ValueError("É impossível calcular 0^0 mod m")
+            return "É impossível calcular 0^0 mod m"
         return 1 % m
     if n % 2 == 0:
         x = exp_mod(a, n // 2, m)
